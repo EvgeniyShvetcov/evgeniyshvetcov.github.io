90955f02869e3d3192dece0dd1c0c14405760b5a
Evgeniy Shvetsov <shvetcovevgeniy@gmail.com>
2017-10-21 18:17:16 +0500

    Delete chapter2.js

--- chapter2.js
+++
@@ -1,9 +0,0 @@
-let topic1 = new Vue({
-    el: "#topic1",
-    data: {
-        currentDate : Date.now(),
-        message :'<p> Hello there!</p>',
-        props : 'Смотри что тут написано',
-        str: 'Странная строка с пробелами'
-    }
-})
