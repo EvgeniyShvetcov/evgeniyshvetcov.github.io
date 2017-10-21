76d50c4811bcff0dd02428ec79b202cd21ea166f
Evgeniy Shvetsov <shvetcovevgeniy@gmail.com>
2017-10-21 18:16:13 +0500

    Add files via upload

---
+++ chapter2.js
@@ -0,0 +1,9 @@
+let topic1 = new Vue({
+    el: "#topic1",
+    data: {
+        currentDate : Date.now(),
+        message :'<p> Hello there!</p>',
+        props : 'Смотри что тут написано',
+        str: 'Странная строка с пробелами'
+    }
+})
